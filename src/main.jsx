339b3289febba9efcd9e7fbc14d48c186f5977e9
SwathiKaruppaiya15 <thesde.swathi1215@gmail.com>
2025-12-18 11:00:16 +0530

    error-fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,10 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter><App /></BrowserRouter>
+    {/* <BrowserRouter><App /></BrowserRouter> */}
+    <BrowserRouter basename="/youtube-clone">
+  <App />
+</BrowserRouter>
+
   </StrictMode>,
 )
